40b8e1ca23bb6a7b84a007fa62c0a3d8871a275b
ajekar <ajekar@gmail.com>
2017-03-15 00:47:51 +0530

    Merge Fixes

--- mergeData.py
+++ mergeData.py
@@ -1,15 +1,15 @@
 import glob
 
-outf = open('E:\Documents\Stock\A-Consolidated.csv', 'w')
+outf = open('D:\Stocks\A-Consolidated.csv', 'w')
 
-for f in glob.glob1("E:\Documents\Stock", "EQ*.CSV"):
+for f in glob.glob1("D:\Stocks", "EQ*.CSV"):
     dt = '20' + f[6:8] + '-' + f[4:6] + '-' + f[2:4]
     print(f, dt)
 
     lineno = 1
-    for line in open("E:\Documents\Stock\\" + f):
+    for line in open("D:\Stocks\\" + f):
         if(lineno > 1):
-            print(dt + "," +line, file = outf)
+            outf.write(dt + "," + line )
         lineno += 1
 
 
