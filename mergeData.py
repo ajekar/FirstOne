dcad3ab908b08e681ac03b91102da175915ae3a7
Sudhir Kumar <sudhirkumar@sintecmedia.com>
2017-03-14 18:31:58 +0530

    New Merge

--- mergeData.py
+++ mergeData.py
@@ -1,8 +1,16 @@
 import glob
 
+outf = open('E:\Documents\Stock\A-Consolidated.csv', 'w')
+
 for f in glob.glob1("E:\Documents\Stock", "EQ*.CSV"):
-    print(f)
-    dd = f[2:4]
-    mm = f[4:6]
-    yy = f[6:8]
-    print('20' + yy + '-' + mm + '-' + dd)
+    dt = '20' + f[6:8] + '-' + f[4:6] + '-' + f[2:4]
+    print(f, dt)
+
+    lineno = 1
+    for line in open("E:\Documents\Stock\\" + f):
+        if(lineno > 1):
+            print(dt + "," +line, file = outf)
+        lineno += 1
+
+
+
